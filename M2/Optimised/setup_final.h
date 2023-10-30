48e2d76b6f9e8491331a28e5e840c6d5e02c7318
jennasupper <jennamsupper@gmail.com>
2023-10-30 10:49:23 +1000

    setup for m2 (no optimisation)

---
+++ M2/Optimised/setup_final.h
@@ -0,0 +1,10 @@
+#define SCALE_FACTOR 2 // set as 0 temporarily
+#define N 1000 // size of the world
+#define L 500 // number of lights
+#define M 10 // number of moths
+#define L_SIZE 2 // size of the light array
+#define M_SIZE 3 // size of the moth array
+
+void generateLuminosity (float lum[N][N], int lights[L][L_SIZE]);
+void generateLights (int pos[L][L_SIZE]);
+void generateMoths (int pos[M][M_SIZE]);
