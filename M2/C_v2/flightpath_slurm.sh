d62319a3bab1f78880dc186c1e0e5a6ea8cd6e93
jennasupper <jennamsupper@gmail.com>
2023-11-02 16:16:30 +1000

    killing myself

--- M2/C_v2/flightpath_slurm.sh
+++ M2/C_v2/flightpath_slurm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name=flightpath
 #SBATCH --time=0-00:59 # time (D-HH:MM)
-#SBATCH --mem=1000 # memory pool for all cores in MB (see also --mem-per-cpu)
+#SBATCH --partition=vgpudev
+#SBATCH --mem=1GB # memory pool for all cores in MB (see also --mem-per-cpu)
 
 time ./flightpath_final
